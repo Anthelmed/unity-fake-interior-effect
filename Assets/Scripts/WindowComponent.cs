20272066bcd586afe59545d6e62cf64a0ce28b78
anthelme_dumont <anthelme.dumont@gmail.com>
2018-10-16 23:57:18 +0200

    first commit

---
+++ Assets/Scripts/WindowComponent.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class WindowComponent : MonoBehaviour
+{
+    [SerializeField]
+    ReflectionProbe reflectionProbeToCopy;
+
+    ReflectionProbe reflectionProbe;
+    MeshRenderer meshRenderer;
+
+    void Awake()
+    {
+        if (reflectionProbeToCopy == null)
+        {
+            Debug.LogWarning("ReflectionProbeToCopy is empty");
+            return;
+        }
+
+        if (reflectionProbe != null)
+            Destroy(reflectionProbe.gameObject);
+
+        CloneReflectionProbe();
+
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.probeAnchor = reflectionProbe.transform;
+    }
+
+    void CloneReflectionProbe()
+    {
+        reflectionProbe = Instantiate(reflectionProbeToCopy, transform.position, transform.rotation, transform);
+        reflectionProbe.bakedTexture = reflectionProbeToCopy.bakedTexture;
+    }
+}
